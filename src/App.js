678031807a4eccaeea74b09df57a5e772a1d0127
Jinto Jose <jintoppy@gmail.com>
2021-08-06 12:44:25 +0530

    fireevent eg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [val, setVal] = useState('');
+
+  const onBtnClick = () => {
+    setVal('hi');
+  };
+
   return (
     <div className="App">
       <h2>Hello</h2>
+      <h3>{val}</h3>
+      <button onClick={onBtnClick}>Click me</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
+describe('<App />', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+  it('renders learn react link', () => {    
+    const linkElement = screen.getByText('Hello');
+    expect(linkElement).toBeInTheDocument();
+  });
 
-test('renders learn react link', () => {
-  render(<App />);  
-  const linkElement = screen.getByText('Hello');
-  expect(linkElement).toBeInTheDocument();
+  it('should set value hi on button click', () => {
+    const btnElement = screen.getByRole('button');
+    fireEvent.click(btnElement);
+    const textElement = screen.getByText('hi');
+    expect(textElement).toBeInTheDocument();
+  });
+  
 });
